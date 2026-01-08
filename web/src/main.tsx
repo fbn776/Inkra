086df82b76870f505f75c8df4f6a53dad080d9c2
fbn776 <fbn776@gmail.com>
2026-01-08 23:44:49 +0530

    feat: not found page

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,14 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
-import { BrowserRouter, Route, Routes } from "react-router";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {BrowserRouter, Route, Routes} from "react-router";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import LoginPage from "./pages/login.tsx";
 import AdminLayout from "./pages/admin/layout.tsx";
 import AdminPage from "./pages/admin/page.tsx";
 import SignDocPage from "./pages/sign-doc.tsx";
 import AdminViewDocPage from "./pages/admin/admin-view.tsx";
 import App from "@/App.tsx";
+import NotFoundPage from "@/pages/not-found.tsx";
 
 const queryClient = new QueryClient({
     defaultOptions: {
@@ -24,16 +25,18 @@ createRoot(document.getElementById('root')!).render(
         <QueryClientProvider client={queryClient}>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<App />} />
+                    <Route path="/" element={<App/>}/>
 
-                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/login" element={<LoginPage/>}/>
 
-                    <Route path="admin" element={<AdminLayout />}>
-                        <Route index element={<AdminPage />} />
-                        <Route path="view/:doc-id" element={<AdminViewDocPage />} />
+                    <Route path="admin" element={<AdminLayout/>}>
+                        <Route index element={<AdminPage/>}/>
+                        <Route path="view/:doc-id" element={<AdminViewDocPage/>}/>
                     </Route>
 
-                    <Route path="/doc/sign/:doc-id" element={<SignDocPage />} />
+                    <Route path="/doc/sign/:doc-id" element={<SignDocPage/>}/>
+
+                    <Route path="*" element={<NotFoundPage/>}/>
                 </Routes>
             </BrowserRouter>
         </QueryClientProvider>
